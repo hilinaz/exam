e40828a2f9ef7cc997d68dadc4f700a4a448fa7f
hilinaz <hiluzemedkun@gmail.com>
2024-05-13 06:47:56 -0700

    Update arraymanipulate.java

--- arraymanipulate.java
+++ arraymanipulate.java
@@ -6,25 +6,23 @@ public class arraymanipulate {
 
 	public static void main(String[] args) {
 		int[]num= {1,2,3,4,5};
-		int []newar=insert(num,3,9);
+		int []newar=delete(num,3,9);
 		System.out.println(Arrays.toString(newar));
 
 	}
-static int[] insert(int[]num,int index,int value) {
-	
-	if(index<0||index>=num.length) {
-		System.out.println("Invalid index");
-		return num;
-		
-	}
-	else {
-		int[] newArray=new int[num.length+1];
-		newArray[index]=value;
-		for(int i=0;i<num.length;i++) {if(i<index) {newArray[i]=num[i];}
-		else if(i>=index) {newArray[i+1]=num[i];}
-		}
-		return newArray;
-	}
-	
+static int[] delete(int[] num, int index) {
+    if (index < 0 || index >= num.length) {
+        System.out.println("Invalid index");
+        return num;
+    } else {
+        int[] newArray = new int[num.length - 1];
+        for (int i = 0, j = 0; i < num.length; i++) {
+            if (i != index) {
+                newArray[j] = num[i];
+                j++;
+            }
+        }
+        return newArray;
+    }
 }
 }
